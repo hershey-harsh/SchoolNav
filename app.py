0c72737d9a4c317fc97d09c1773b1cb5c15cfc59
Harsh <77411358+hershey-harsh@users.noreply.github.com>
2024-06-23 21:51:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
-from werkzeug.exceptions import HTTPException
+
+from routes.grades import grades_page
+from api_routes.grades import grades_api
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
+
+# Pages Routes
+app.register_blueprint(grades_page)
+
+# Backend Routes
+app.register_blueprint(grades_api)
+
+# Import after initialization of routes
+from werkzeug.exceptions import HTTPException
 
 @app.errorhandler(Exception)
 def handle_exception(e):
